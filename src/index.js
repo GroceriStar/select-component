408d9a4df95325c70394737f65dfd817e9f64ece
Arthur Tkachenko <atherdon@gmail.com>
2018-12-13 06:54:28 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import TodaysDate  from '~/TodaysDate'
+import List        from '~/List/List'
+import DefaultList from '~/List/DefaultList'
+
+// import List from './List/List';
+// import DefaultList from './List/DefaultList';
+// import TodaysDate from './TodaysDate';
+// import List from './List/List';
+// import DefaultList from './List/DefaultList';
+
+export {
+  TodaysDate,
+  List,
+  DefaultList
+};
