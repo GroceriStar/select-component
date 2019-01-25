bbfe74febd10cf56ef7a9dfc77ff6b70f01200b3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-25 09:29:12 +0200

    +

--- src/components/GroupedElements/GroupedElements.test.js
+++ src/components/GroupedElements/GroupedElements.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, {shallow, configure } from 'enzyme';
+import Enzyme, { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { GroupedElements } from '~/GroupedElements/GroupedElements';
 
